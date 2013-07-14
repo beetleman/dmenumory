fdf77fb6b0c189b80b88e0317d13314a630bed50
beetleman <nigrantis.tigris@gmial.com>
2013-07-14 05:17:58 +0200

    speed up: now use file cache

--- dmenumory.py
+++ dmenumory.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import os
-from dmenu import Dmenu
 import subprocess
 import json
 from contextlib import contextmanager
 
+from dmenu import Dmenu
 
 CACHE_FILE = "/home/robal/.dmenumory"
 
@@ -26,9 +26,11 @@ def auto_save_dict(filename):
         json.dump(data,fobj)
         fobj.truncate()
 
+
 def get_zero():
     while True:
         yield 0
+
 
 class Dmenumory(object):
     def __init__(self,cache_file_name, options={}):
@@ -44,26 +46,33 @@ class Dmenumory(object):
                                               os.X_OK)])
         return list(all_exec)
 
+
     def _get_new_cache(self):
-        return dict(zip(self._get_app_list(),get_zero()))
+        return dict(zip(self._get_app_list(), get_zero()))
+
+    def _cache_to_app_list(self, cache):
+        return sorted(cache, lambda x,y: cache[y] - cache[x])
+
+    def _update_cache(self, cache):
+        new_cache = self._get_new_cache()
+        diff_old = set(cache).difference(set(new_cache))
+        diff_new = set(new_cache).difference(set(cache))
+        for app in diff_old:
+            del cache[app]
+        for app in diff_new:
+            cache[app] = 0
 
     def run(self):
-        app_list = self._get_app_list()
         with auto_save_dict(self.cache_file_name) as cache:
-            for app in cache:
-                try:
-                    app_list.remove(app)
-                except ValueError:
-                    pass
-            print cache
-            app_list = sorted(cache, lambda x,y: cache[y] - cache[x]) + app_list
-
+            if not cache:
+                cache.update(self._get_new_cache())
             def runit(app):
                 if app:
                     cache[app] = 1+ cache.get(app, 0)
                     subprocess.Popen(app)
+                    self._update_cache(cache)
 
-            dmenu = Dmenu(app_list, runit)
+            dmenu = Dmenu(sorted(cache, lambda x,y: cache[y] - cache[x]), runit)
             dmenu.set_options(**self.options)
             dmenu.run()
 
