f5bbd565fae1cc20208ef7c8ac14a94252b167ce
beetleman <nigrantis.tigris@gmial.com>
2014-05-17 21:13:11 +0200

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='Dmenumory',
-    version='0.1',
+    version='0.2',
     url='https://github.com/beetleman/dmenumory',
     description='Launcher build on top of dmenu  with simple launch statistic.',
     author='Mateusz Probachta aka beetleman',
-    author_email='nigrantis.tigris@gmail.com',
+    author_email='mateusz.probachta@gmail.com',
     license="LGPL",
     keywords="dmenu launcher",
     long_description=open('README').read(),
